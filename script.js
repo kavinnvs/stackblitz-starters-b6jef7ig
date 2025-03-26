5df242aa9b47eea87d8849a9c8146627776f3267
vs.kavin <kavinvs90@gmail.com>
2025-03-26 09:43:50 +0530

    day

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Function to get the value of a cookie by name
-  function getCookie(name) {
-    let cookieArray = document.cookie.split('; ');
-    let cookie = cookieArray.find((row) => row.startsWith(name + '='));
-    return cookie ? cookie.split('=')[1] : null;
+function getCookie(name) {
+  let cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+      let [key, value] = cookie.split("=");
+      if (key === name) {
+          return value;
+      }
   }
+  return null;
+}
 
-  // Function to set a cookie
-  function setCookie(name, value, daysToExpire) {
-    let date = new Date();
-    date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
-    document.cookie =
-      name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
   }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
 
-  // 1. Get the value of the 'count' cookie
-  // 2. If the cookie exists, increment the value and update the cookie
-  // 3. If the cookie does not exist, create it and set the value to 1
-  // 4. Display the count on the webpage
 
-  // your code here
-});
+function updateCounter() {
+  let count = getCookie("pageCount");
+  count = count ? parseInt(count) + 1 : 1; 
+  setCookie("pageCount", count, 7); 
+  document.getElementById("countDisplay").innerText = "Page Views: " + count;
+}
+
+
+updateCounter();
